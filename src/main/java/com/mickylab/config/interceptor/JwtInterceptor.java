206b7d369defa63c629719936b2e73f283738b99
mickylab <42708694+mickylab@users.noreply.github.com>
2022-05-13 12:10:37 -0400

    add jwt support

--- src/main/java/com/mickylab/config/interceptor/JwtInterceptor.java
+++ src/main/java/com/mickylab/config/interceptor/JwtInterceptor.java
@@ -1,2 +1,53 @@
-package com.mickylab.config.interceptor;public class JwtInterceptor {
+package com.mickylab.config.interceptor;
+
+import cn.hutool.core.util.StrUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.mickylab.common.Constants;
+import com.mickylab.entity.User;
+import com.mickylab.exception.ServiceException;
+import com.mickylab.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class JwtInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String token = request.getHeader("token");
+        // 如果不是映射到方法直接通过
+        if (!(handler instanceof HandlerMethod)) return true;
+        // 执行认证
+        if (StrUtil.isBlank(token)) throw new ServiceException(Constants.CODE_401, "No token, please log in again");
+        // 获取 token 中的 user id
+        String userId;
+        try {
+            userId = JWT.decode(token).getAudience().get(0);
+        } catch (JWTDecodeException j) {
+            throw new ServiceException(Constants.CODE_401, "Token verification failed, please log in again");
+        }
+        // 根据token中的userid查询数据库
+        User user = userServiceImpl.getById(userId);
+        if (user == null) {
+            throw new ServiceException(Constants.CODE_401, "User does not exist, please log in again");
+        }
+        // 用户密码加签验证 token
+        JWTVerifier jwtVerifier = JWT.require(Algorithm.HMAC256(user.getPassword())).build();
+        try {
+            jwtVerifier.verify(token); // 验证token
+        } catch (JWTVerificationException e) {
+            throw new ServiceException(Constants.CODE_401, "Token verification failed, please log in again");
+        }
+        return true;
+    }
 }
--- src/main/java/com/mickylab/utils/TokenUtils.java
+++ src/main/java/com/mickylab/utils/TokenUtils.java
@@ -1,2 +1,59 @@
-package com.mickylab.utils;public class TokenUtils {
+package com.mickylab.utils;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.mickylab.entity.User;
+import com.mickylab.service.IUserService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.Objects;
+
+@Component
+public class TokenUtils {
+
+    private static IUserService staticUserService;
+
+    @Resource
+    private IUserService userService;
+
+    @PostConstruct
+    public void setUserService() {
+        staticUserService = userService;
+    }
+
+    /**
+     * 生成token
+     * @return token
+     */
+    public static String getToken(String userId, String sign) {
+        return JWT.create().withAudience(userId) // 将 userId 保存到 token 里面, 作为载荷
+                .withExpiresAt(DateUtil.offsetHour(new Date(), 2)) // 2小时后token过期
+                .sign(Algorithm.HMAC256(sign)); // 以 password 作为 token 的密钥
+    }
+
+    /**
+     * 获取当前登录的用户信息 后台获取 可在全局使用
+     * @return user 对象
+     */
+    public static User getCurrentUser() {
+        try {
+            HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+            String token = request.getHeader("token");
+            if (StrUtil.isNotBlank(token)) {
+                String userId = JWT.decode(token).getAudience().get(0);
+                return staticUserService.getById(Integer.valueOf(userId));
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        return null;
+    }
 }
